67217ddfda23a39254ac20144ece48cd0728f13c
Osman Kuru <osmankuru2006@gmail.com>
2025-03-19 17:55:24 +0300

    Parent zombie class and player class added

--- Source/GamePanel.java
+++ Source/GamePanel.java
@@ -4,27 +4,44 @@ import java.awt.Graphics;
 import java.awt.Image;
 
 import java.io.File;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
+import Source.Zombies.ParentZombie;
+
 public class GamePanel extends JPanel {
     private Image backgroundImage;
+    private GameLoop loop;
+    ArrayList<ParentZombie> zombies = new ArrayList<>();
+    Player player;
 
     GamePanel() {
+        this.setSize(GameFrame.WIDTH, GameFrame.HEIGHT);
+        loop = new GameLoop(this);
+        player = new Player("Player1", 100, GameFrame.WIDTH / 2, GameFrame.HEIGHT / 2);
+        zombies = loop.addZombies();
+
+        System.out.println(GameFrame.WIDTH);
         try {
             backgroundImage = ImageIO.read(new File(
                     "C:\\Users\\osman\\OneDrive\\Desktop\\OsmanKuru_241101005_Bil211_Ödev\\Source\\images\\backgroundImage.jpg"));
         } catch (Exception e) {
             System.out.println("unable to find file");
         }
-
+        this.repaint();
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+        g.drawImage(backgroundImage, 0, 0, this);
+        player.paint(g);
+        for (ParentZombie zombie : zombies) {
+            zombie.draw(g);
+        }
 
         // Draw the background image.
-        g.drawImage(backgroundImage, 0, 0, this);
+        //
     }
 }
--- Source/Zombies/ParentZombie.java
+++ Source/Zombies/ParentZombie.java
@@ -2,8 +2,48 @@
 
 package Source.Zombies;
 
-public class ParentZombie {
-    public static void main(String[] args) {
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
 
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+public class ParentZombie {
+    int health;
+    double speed;
+    private static final int diameter = 36;
+    int damage;
+    int xCoordinate;
+    int yCoordinate;
+    double centerX;
+    double centerY;
+    Color color;
+    private Image image;
+
+    public ParentZombie(int health, double speed, int damage, int xCoordinate, int yCoordinate) {
+        this.health = health;
+        this.speed = speed;
+        this.damage = damage;
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+        centerX = xCoordinate + (diameter / 2);
+        centerY = yCoordinate + (diameter / 2);
+        try {
+            image = ImageIO.read(new File(
+                    "C:\\Users\\osman\\OneDrive\\Desktop\\OsmanKuru_241101005_Bil211_Ödev\\Source\\images\\NormalZombie.png"));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
+
+    public void draw(Graphics g) {
+        g.drawImage(image, xCoordinate, yCoordinate, new JPanel());
+    }
+
+    public void repaint(Graphics g) {
+        draw(g);
+    }
+
 }
